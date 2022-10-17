87e544d2d7f5651ad24c24e53f3069501df002d4
fadl8 <fadhl2020nasser@gmail.com>
2022-10-17 03:25:43 -0700

    Adding the Codes to downleadPDF File

--- GemBoxProject/Program.cs
+++ GemBoxProject/Program.cs
@@ -1,4 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+// the Key
+using GemBox.Document;
+
+ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+DocumentModel document = new DocumentModel();
+
+Section section = new Section(document);
+document.Sections.Add(section);
+
+Paragraph paragraph = new Paragraph(document);
+section.Blocks.Add(paragraph);
+
+Run run = new Run(document, "Hello World!");
+paragraph.Inlines.Add(run);
+
+// link
+// https://www.gemboxsoftware.com/pdf/examples/c-sharp-vb-net-pdf-library/101
 Console.WriteLine("Hello, World!");
 
